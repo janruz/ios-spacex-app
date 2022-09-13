6eacf3e73d15ad7b30d497182913b0d25c2c2c60
Jan Ruzicka <ho.ruz@seznam.cz>
2022-09-13 16:44:25 +0200

    Add navigation controller

---
+++ SpaceX App/LaunchesViewController.swift
@@ -0,0 +1,71 @@
+//
+//  LaunchesViewController.swift
+//  SpaceX App
+//
+//  Created by Jan Růžička on 11.09.2022.
+//
+
+import UIKit
+
+fileprivate let reuseIdentifier = "rocketLaunchCell"
+
+class LaunchesViewController: UIViewController {
+    
+    private var rocketLaunches = [RocketLaunch]() {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .singleLine
+        tableView.register(RocketLaunchCell.self, forCellReuseIdentifier: reuseIdentifier)
+        tableView.dataSource = self
+        
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = "Rocket launches"
+        layout()
+        
+        Task {
+            let result = await RocketLaunchesRepository.shared.getLaunches()
+            
+            switch result {
+            case .success(let launches):
+                self.rocketLaunches = launches
+            case .failure(_):
+                break
+            }
+        }
+    }
+    
+    private func layout() {
+        view.addSubview(tableView)
+        tableView.constrain(
+            top: view.topAnchor,
+            leading: view.safeAreaLayoutGuide.leadingAnchor,
+            bottom: view.bottomAnchor,
+            trailing: view.safeAreaLayoutGuide.trailingAnchor
+        )
+    }
+}
+
+extension LaunchesViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rocketLaunches.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! RocketLaunchCell
+        cell.rocketLaunch = rocketLaunches[indexPath.row]
+        return cell
+    }
+}
--- SpaceX App/MainViewController.swift
+++ SpaceX App/MainViewController.swift
@@ -1,71 +1,28 @@
 //
-//  MainViewController.swift
+//  MainController.swift
 //  SpaceX App
 //
-//  Created by Jan Růžička on 11.09.2022.
+//  Created by Jan Růžička on 13.09.2022.
 //
 
+import Foundation
 import UIKit
-import Alamofire
 
-fileprivate let reuseIdentifier = "rocketLaunchCell"
-
-class MainViewController: UIViewController {
+class MainViewController: UINavigationController {
     
-    private var rocketLaunches = [RocketLaunch]() {
-        didSet {
-            tableView.reloadData()
-        }
+    //MARK: - Lifecycle
+    
+    init() {
+        super.init(rootViewController: LaunchesViewController())
     }
     
-    private lazy var tableView: UITableView = {
-        let tableView = UITableView()
-        tableView.rowHeight = UITableView.automaticDimension
-        tableView.separatorStyle = .singleLine
-        tableView.register(RocketLaunchCell.self, forCellReuseIdentifier: reuseIdentifier)
-        tableView.dataSource = self
-        
-        return tableView
-    }()
-
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        layout()
-        
-        Task {
-            let result = await RocketLaunchesRepository.shared.getLaunches()
-            
-            switch result {
-            case .success(let launches):
-                self.rocketLaunches = launches
-            case .failure(_):
-                break
-            }
-        }
-    }
-    
-    private func layout() {
-        view.addSubview(tableView)
-        tableView.constrain(
-            top: view.topAnchor,
-            leading: view.safeAreaLayoutGuide.leadingAnchor,
-            bottom: view.bottomAnchor,
-            trailing: view.safeAreaLayoutGuide.trailingAnchor
-        )
-    }
-}
-
-extension MainViewController: UITableViewDataSource {
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return rocketLaunches.count
-    }
-    
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! RocketLaunchCell
-        cell.rocketLaunch = rocketLaunches[indexPath.row]
-        return cell
+        navigationBar.prefersLargeTitles = true
     }
 }
